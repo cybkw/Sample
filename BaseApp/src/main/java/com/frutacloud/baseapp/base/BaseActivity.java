d5ddd70757d8c7c5cb946ea50220c5071d359095
CLW_Kaiwen_Bai <kaiwen.bai@cloudinward.com>
2017-11-07 14:02:52 +0800

    在基类Activity中加入右滑退出Activity支持

--- BaseApp/src/main/java/com/frutacloud/baseapp/base/BaseActivity.java
+++ BaseApp/src/main/java/com/frutacloud/baseapp/base/BaseActivity.java
@@ -1,24 +1,30 @@
 package com.frutacloud.baseapp.base;
 
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.support.v4.widget.SlidingPaneLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
 
-import com.android.pc.ioc.inject.InjectInit;
+import com.frutacloud.baseapp.R;
 import com.frutacloud.baseapp.netbase.NetField;
 import com.frutacloud.baseapp.utils.Tools;
 import com.frutacloud.baseapp.weight.XLoadingDialog;
 import com.frutacloud.baseapp.weight.XToast;
 
 import java.lang.ref.WeakReference;
+import java.lang.reflect.Field;
 
 /**
+ * 基类Activity
  * Created by Administrator on 2017/11/2.
  */
 
-public abstract class BaseActivity extends AppCompatActivity {
+public abstract class BaseActivity extends AppCompatActivity implements SlidingPaneLayout.PanelSlideListener {
 
     /* 上下文*/
     protected Activity mActivity;
@@ -28,7 +34,9 @@ public abstract class BaseActivity extends AppCompatActivity {
 
     @InjectInit
     void init() {
+
         mActivity = this;
+        initSwipeBackFinish();
     }
 
     /**
@@ -99,6 +107,65 @@ public abstract class BaseActivity extends AppCompatActivity {
      */
     protected void dismissXloading() {
         XLoadingDialog.with(this).dismiss();
+    }
+
+    /**
+     * 初始化滑动返回
+     */
+    private void initSwipeBackFinish() {
+        if (isSupportSwipeBack()) {
+            SlidingPaneLayout slidingPaneLayout = new SlidingPaneLayout(this);
+            //通过反射改变mOverhangSize的值为0，这个mOverhangSize值为菜单到右边屏幕的最短距离，默认
+            //是32dp，现在给它改成0
+            try {
+                //属性
+                Field f_overHang = SlidingPaneLayout.class.getDeclaredField("mOverhangSize");
+                f_overHang.setAccessible(true);
+                f_overHang.set(slidingPaneLayout, 0);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            slidingPaneLayout.setPanelSlideListener(this);
+            slidingPaneLayout.setSliderFadeColor(getResources().getColor(android.R.color.transparent));
+
+            View leftView = new View(this);
+            leftView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+            slidingPaneLayout.addView(leftView, 0);
+
+            ViewGroup decor = (ViewGroup) getWindow().getDecorView();
+            ViewGroup decorChild = (ViewGroup) decor.getChildAt(0);
+            decorChild.setBackgroundColor(getResources().getColor(android.R.color.white));
+            decor.removeView(decorChild);
+            decor.addView(slidingPaneLayout);
+            slidingPaneLayout.addView(decorChild, 1);
+        }
+    }
+
+    /**
+     * 是否支持滑动返回
+     *
+     * @return
+     */
+    protected boolean isSupportSwipeBack() {
+        return true;
+    }
+
+    @Override
+    public void onPanelClosed(View view) {
+
+    }
+
+    @Override
+    public void onPanelOpened(View view) {
+        finish();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ECLAIR) {
+            this.overridePendingTransition(0, R.anim.slide_out_right);
+        }
+    }
+
+    @Override
+    public void onPanelSlide(View view, float v) {
+
     }
 
     protected class HandlerRequest extends Handler {
